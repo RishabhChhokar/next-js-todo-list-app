1d0cca9cd3d9d6fab96f696fa7cdeb4b464d97e7
Rishabh Chhokar <rishabhchokkar@gmail.com>
2024-05-14 21:50:34 +0530

    Initiliazed todo list app ,installed mogoose package and added css for later use.

---
+++ src/app/components/PlusIcon.jsx
@@ -0,0 +1,21 @@
+const PlusIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M4 12h16m-8-8v16"
+      ></path>
+    </svg>
+  );
+}
+
+export default PlusIcon;
